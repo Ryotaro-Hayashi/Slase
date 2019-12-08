558728f3bbedc348b74869bd18eeb81f1b681d64
hysrtr <hys.rtr.forit>
2019-12-08 20:31:58 +0900

    [add] createアクションを追加

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,23 @@
-class UsersController < ApplicationController
+module Api
+  module V1
+    class UsersController < ApplicationController
+      # ユーザーの作成と保存
+      def create
+        user = User.new(user_params)
+        if user.save
+          render json: user
+        else
+          render json: user.errors
+        end
+      end
+
+      # 外部から使えないように制限
+      private
+      # Strong Parametersを使い、必須のパラメータと許可されたパラメータを指定
+      def user_params
+        # userモデルを必須にして、name,emailカラムを許可
+        params.require(:user).permit(:name, :email, :password)
+      end
+    end
+  end
 end
