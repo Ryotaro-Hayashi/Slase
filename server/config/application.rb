f66a1ca546ae1f1d23d04f219b4a8adbb46952e7
hysrtr <hys.rtr.forit>
2020-01-03 03:03:38 +0900

    Add：rack-corsを入れたことによる設定変更

--- server/config/application.rb
+++ server/config/application.rb
@@ -31,5 +31,18 @@ module Slase
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators.system_tests = nil
+
+    if Rails.env.development?
+      config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+        allow do
+          origins '*'
+          resource '*',
+            :headers => :any,
+            :methods => [:get, :post, :options, :patch, :delete]
+        end
+      end
+    end
   end
 end
