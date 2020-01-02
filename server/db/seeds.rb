377969733676b47f11412ade96cc28294554a985
hysrtr <hys.rtr.forit>
2020-01-03 03:23:32 +0900

    Add：seeds.rbにサンプルユーザーを入れて、rails db:seed

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# サンプルユーザーの作成
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar")
