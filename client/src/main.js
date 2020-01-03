c37548be34d127f70272093def61617638dcf54f
hysrtr <hys.rtr.forit>
2020-01-04 06:14:16 +0900

    Change：ファイルの移動

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import axios from 'axios'
+import App from './App'
+import router from './router'
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
+  // el: '#app',
+  router,
   vuetify,
+  axios,
   render: h => h(App)
-}).$mount('#app')
+  // components: { App },
+  // template: '<App/>'
+})
+.$mount('#app')
