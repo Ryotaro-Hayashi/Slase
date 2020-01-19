41db7b9af168c457d865f4fc7c73b566597c112b
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-01-19 21:59:13 +0900

    Change：データベースの設定を改良（ . ではなく _ に変更）
    Command：rails db:create/ rails db:migrate

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2019_12_08_105622) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password"
