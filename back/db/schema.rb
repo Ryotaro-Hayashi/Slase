5d15c6ab1e156f01cc7e2be93d27b02962bce329
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-01-23 02:06:50 +0900

    Change：passwordの変更を可能にしてから、rails db:migrate:reset

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_01_21_155916) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
