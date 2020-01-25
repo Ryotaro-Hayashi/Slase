baad0c7d13c504e272d652f2b88e9fa2423a793b
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-01-25 20:48:45 +0900

    Fix：コンポーネントのメソッドでstoreのアクションを呼び出す

--- front/src/views/Signup.vue
+++ front/src/views/Signup.vue
@@ -20,14 +20,15 @@
       <v-form>
         <!-- iライブラリをインストールしているので、mdiを使える -->
         <!-- マテリアルデザインアイコンを使うときは、アイコン名の先頭に「mdi-」を付ける -->
-        <v-text-field prepend-icon="mdi-account-circle" label="ユーザ名" v-model="data.name" />
-        <EmailForm></EmailForm>
+        <v-text-field prepend-icon="mdi-account-circle" label="ユーザ名" v-model="name" />
+        <!-- <EmailForm></EmailForm> -->
+        <v-text-field prepend-icon="mdi-email" label="メールアドレス" v-model="email" />
         <!-- type="password"を入れて、入力内容を隠す -->
         <!-- prepend-icon で前に、append-icon で後ろにアイコンを配置-->
         <!-- showPasswordプロパティの真偽で、属性typeがtextとpasswordに切り替わるようにする -->
         <!-- クリックイベントを追加 -->
         <!-- showPasswordプロパティの真偽で、アイコンを変更するようにする -->
-        <v-text-field v-bind:type="showPassword ? 'text' : 'password'" prepend-icon="mdi-lock" label="パスワード" v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" @click:append="showPassword = !showPassword" v-model="data.password" />
+        <v-text-field v-bind:type="showPassword ? 'text' : 'password'" prepend-icon="mdi-lock" label="パスワード" v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" @click:append="showPassword = !showPassword" v-model="password" />
         <!-- ユーザー登録ボタンの配置エリア -->
         <v-card-actions class="mt-5">
           <!-- vuetifyクラスでボタンの色を変更 -->
@@ -41,39 +42,28 @@
 </template>
 
 <script>
-import EmailForm from '@/components/EmailForm.vue'
+// import EmailForm from '@/components/EmailForm.vue'
 
 export default {
   name: 'Signup',
-  components: {
-    EmailForm
-  },
+  // components: {
+  //   EmailForm
+  // },
   data: () => ({
     // showPasswordプロパティでパスワードの表示・非表示を切り替える
     showPassword: false,
-    data: {
-      name: '',
-      email: '',
-      password: ''
-    },
+    name: '',
+    email: '',
+    password: '',
     Tokens: {}
   }),
   methods: {
     register () {
-      this.$axios.post('http://localhost:3000/api/auth', this.data)
-      .then(response => {
-        if (response.status === 200) {
-          this.Tokens = {
-            accessToken: response.headers["access-token"],
-            client: response.headers.client,
-            uid: response.headers.uid
-          }
-          this.$router.push("/mypage")
-        }
+      this.$store.dispatch("signup", {
+        name: this.name,
+        email: this.email,
+        password: this.password
       })
-      // .catch(error => {
-      //   this.$router.push("/users")
-      // })
     }
   }
 }
