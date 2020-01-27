09f425732d3dda5ca50f4c682c5381ea81949936
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-01-27 19:09:55 +0900

    Add：questionのカラムとしてtitleとbodyを追加してマイグレート

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_155916) do
+ActiveRecord::Schema.define(version: 2020_01_27_094824) do
+
+  create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "provider", default: "email", null: false
