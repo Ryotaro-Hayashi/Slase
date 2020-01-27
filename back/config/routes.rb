955e6049d84206cee9ae1226af4f054400f7a645
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-01-27 18:58:14 +0900

    Add：questions用のルーティングを追加

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   namespace 'api' do
+
     mount_devise_token_auth_for 'User', at: 'auth'
+
+    namespace 'post' do
+
+      resources :questions
+      
+    end
   end
 end
