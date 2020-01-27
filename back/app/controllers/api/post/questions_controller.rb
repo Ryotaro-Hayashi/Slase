178cd635d4ca3535ad6ad1c42d5b19351d0ebd1c
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-01-27 19:24:42 +0900

    Add：質問を作成してjsonで返す

--- back/app/controllers/api/post/questions_controller.rb
+++ back/app/controllers/api/post/questions_controller.rb
@@ -1,3 +1,19 @@
 class Api::Post::QuestionsController < ApplicationController
-  
+  def create
+    question = Question.new(question_params)
+    if question.save
+      render json: question
+    end
+  end
+
+  # def index
+  #   questions = Question.all
+  #   render json: questions
+  # end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 end
