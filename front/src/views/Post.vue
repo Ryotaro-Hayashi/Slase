2abb916c6363f88f33d3c846e90a12bfe6a82f39
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-01-30 18:44:34 +0900

    Fix：textareaにheight指定

--- front/src/views/Post.vue
+++ front/src/views/Post.vue
@@ -6,8 +6,8 @@
       <v-card width="1200px" class="mx-auto">
 
         <v-form>
-          <v-text-field label="タイトル"></v-text-field>
-          <v-text-field label="本文"></v-text-field>
+          <v-text-field label="タイトル" outlined></v-text-field>
+          <v-textarea label="本文" outlined height="700"></v-textarea>
           <v-card-actions class="mt-5">
             <v-btn class="info ml-auto">投稿</v-btn>
           </v-card-actions>
