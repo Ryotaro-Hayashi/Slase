5e2ec2bad2a4e4a7aa2e5c36946cbe0815cc6342
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-01-30 13:16:18 +0900

    Add：Postコンポーネントを追加

---
+++ front/src/views/Post.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1 class="center">Home</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Post'
+}
+</script>
+
+<style scoped>
+
+</style>
