2b464a47c7b384fcc544382ccdda1d85fef5be8f
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-02-01 15:46:12 +0900

    Add：全ての投稿を表示するコンポーネントを追加

---
+++ front/src/views/AllPosts.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="posts">
+    <h1>"allposts"</h1>
+    <p>{{ allQuestions }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AllPosts',
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+    allQuestions () {
+      return this.$store.state.questions
+    },
+    userToken () {
+      return this.$store.state.token
+    }
+  },
+  methods: {
+    questions () {
+      this.$store.dispatch("posts", {
+        token: this.userToken
+      })
+    }
+  },
+  mounted: function() {
+    this.questions()
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
