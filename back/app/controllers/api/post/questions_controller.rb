ba97ada1e310be824c0a93680ec6868e279f3533
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-02-01 17:13:03 +0900

    Add：questionsコントローラーのshowアクションを追加

--- back/app/controllers/api/post/questions_controller.rb
+++ back/app/controllers/api/post/questions_controller.rb
@@ -18,6 +18,11 @@ module Api
         render json: questions
       end
 
+      def show
+        question = Question.find(params[:id])
+        render json: question
+      end
+
       private
 
       def question_params
