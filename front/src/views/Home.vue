30e0640ffc6ed28ac103103f497723f8662ce561
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-02-02 14:41:54 +0900

    Add：リンクをクリックしたらアクションを呼び出し
    router-link は@click.native としないと、クリックイベントが動作しない

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -7,7 +7,7 @@
         </v-card-title>
         <v-card-text>
           <ul>
-            <li v-for="question in allQuestions" :key="question.id"><div class="title font-weight-bold">{{ question.title }}</div>by {{ question.user_id }}  {{ question.created_at}}<v-divider></v-divider></li>
+            <li v-for="question in allQuestions" :key="question.id"><router-link class="title font-weight-bold" to="posting" @click.native="getId(question.id)">{{ question.title }}</router-link><br>by {{ question.user_id }}  {{ question.created_at}}<v-divider></v-divider></li>
             <li v-for="question in allQuestions" :key="question.id">{{ question }}</li>
           </ul>
         </v-card-text>
@@ -22,20 +22,19 @@ export default {
   name: 'Home',
   data () {
     return {
-
     }
   },
   computed: {
     allQuestions () {
       return this.$store.state.questions
-    },
-    userToken () {
-      return this.$store.state.token
     }
   },
   methods: {
     questions () {
       this.$store.dispatch("posts")
+    },
+    getId (id) {
+      this.$store.dispatch("myposts", id)
     }
   },
   mounted: function() {
