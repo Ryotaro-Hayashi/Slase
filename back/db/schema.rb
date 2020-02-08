8cfc707504e4f1cc0de43982799be19363a71d8f
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-02-08 19:37:24 +0900

    Command：rails db:migrate:reset
    userモデルのimageを削除

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_120829) do
+ActiveRecord::Schema.define(version: 2020_01_27_094824) do
 
   create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 2020_02_06_120829) do
     t.text "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
