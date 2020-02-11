e463bbbf8d36851ff25ef3029135624da3c71f8c
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-02-12 02:47:59 +0900

    Add：テーブルにimageを追加してマイグレーション

--- back/db/migrate/20200127094824_create_questions.rb
+++ back/db/migrate/20200127094824_create_questions.rb
@@ -7,6 +7,7 @@ class CreateQuestions < ActiveRecord::Migration[5.2]
 
       t.string :date
       t.string :time
+      t.string :image, default: ""
 
       # 誰が投稿したかが分かるようにuser.idを追加する
       t.references :user
