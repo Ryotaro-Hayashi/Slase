b5e3aa5aa0c7e89e2989a2816e64a947b9fee905
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-02-11 23:24:12 +0900

    Add：メソッドを追加してプレビュー

--- front/src/views/Post.vue
+++ front/src/views/Post.vue
@@ -27,6 +27,7 @@ export default {
   data: () => ({
     title: '',
     body: '',
+    question_image: ''
   }),
   computed: {
     // トークンを取得
@@ -42,6 +43,25 @@ export default {
         body: this.body,
         token: this.userToken
       })
+    },
+    getBase64 (file) {
+      return new Promise((resolve) => {
+        // FileReaderはファイルの読み取りアクセスを行うオブジェクト
+        const reader = new FileReader()
+        // ファイルをDataURIとして読み込むメソッドで、img要素のsrc属性に指定すればブラウザに表示できる。
+        reader.readAsDataURL(file)
+        // onloadメソッドは、読み込みが完了した時に発火するメソッド
+        // resolveは同期処理の成功時の処理を書き、thenに渡される
+        reader.onload = () => resolve(reader.result)
+      })
+    },
+    onImageChange(e) {
+      // filesプロパティは複数ファイルを管理できるように配列になっている
+      const images = e.target.files
+      // ドラッグアンドドロップも有効にするなら || e.dataTransfer.files を追加
+      this.getBase64(images[0])
+        // imageはresolveで渡されたreader.result
+       .then(image => this.question_image = image)
     }
   }
 }
