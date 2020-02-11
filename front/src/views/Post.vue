546759f5d49e1c939eef739a5d8069293511927e
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-02-12 04:15:01 +0900

    Fix：ファイルの読み込みを修正
    今までのエラーはここが原因だったんじゃないかな

--- front/src/views/Post.vue
+++ front/src/views/Post.vue
@@ -8,7 +8,8 @@
           <v-text-field label="タイトル" outlined class="posting" v-model="title"></v-text-field>
           <v-textarea label="本文" outlined height="500" class="posting" v-model="body"></v-textarea>
           <input type="file" accept="image/jpeg, image/png" @change="onImageChange"/>
-          <v-img :src="imageURL" />
+          <v-img :src="imageUrl" />
+          <p>{{imagefile}}</p>
           <!-- スペースを用意 -->
           <v-card-actions class="mt-5">
             <v-btn class="info ml-auto" v-on:click="question">投稿</v-btn>
@@ -34,6 +35,9 @@ export default {
     // トークンを取得
     userToken () {
       return this.$store.state.token
+    },
+    imagefile () {
+      return this.imageFile
     }
   },
   methods: {
@@ -46,24 +50,18 @@ export default {
         token: this.userToken
       })
     },
-    getBase64 (file) {
-      return new Promise((resolve) => {
-        // FileReaderはファイルの読み取りアクセスを行うオブジェクト
-        const reader = new FileReader()
-        // ファイルをDataURIとして読み込むメソッドで、img要素のsrc属性に指定すればブラウザに表示できる。
-        reader.readAsDataURL(file)
-        // 読み込み（load）終わったら中身が発火する
-        reader.addEventListener("load", () => {
-          this.imageUrl = reader.result
-          this.imageFile = file[0]
-        })
-      })
-    },
     onImageChange(e) {
       // filesプロパティは複数ファイルを管理できるように配列になっている
-      const images = e.target.files
-      // ドラッグアンドドロップも有効にするなら || e.dataTransfer.files を追加
-      this.getBase64(images[0])
+      const files = e.target.files
+      // FileReaderはファイルの読み取りアクセスを行うオブジェクト
+      const fr = new FileReader()
+      // ファイルをDataURIとして読み込むメソッドで、img要素のsrc属性に指定すればブラウザに表示できる。
+      fr.readAsDataURL(files[0])
+      // 読み込み（load）終わったら中身が発火する
+      fr.addEventListener("load", () => {
+        this.imageUrl = fr.result
+        this.imageFile = files[0]
+      })
     }
   }
 }
