fc29b63d86e58e9c02c0fa835ef5d4d47051f985
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-02-16 23:44:01 +0900

    Add：フォームなどページの作り込み

--- front/src/views/Email.vue
+++ front/src/views/Email.vue
@@ -1,10 +1,38 @@
 <template>
-<p>Email</p>
+<div class="email">
+  <v-app>
+    <v-card width="600px" class="mx-auto mt-10">
+      <v-card-title>
+        <p class="mx-auto">メールアドレスを変更</p>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <p>現在のメールアドレス：{{ loggedInUserInfo.email }}</p>
+          <v-text-field prepend-icon="mdi-email" label="新しいメールアドレス" v-model="email" />
+          <v-card-actions class="mt-5">
+            <v-btn class="info ml-auto">変更</v-btn>
+          </v-card-actions>
+          {{ email }}
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-app>
+</div>
 </template>
 
 <script>
 export default {
   name: 'Email',
+  data () {
+    return {
+      email: ''
+    }
+  },
+  computed: {
+    loggedInUserInfo () {
+      return this.$store.getters.loggedInUserInfo.user
+    }
+  }
 }
 
 </script>
