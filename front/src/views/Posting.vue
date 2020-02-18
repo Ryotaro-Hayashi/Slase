d39a6885a7d611caef8dc8e098b14ce74ba89d07
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-02-18 23:58:33 +0900

    Add：アイコンの追加とタイトルエリアを作成

--- front/src/views/Posting.vue
+++ front/src/views/Posting.vue
@@ -2,12 +2,22 @@
 <div class="posting">
   <v-app>
     <v-card width="800px" class="mx-auto mt-10">
-      <ul>
-        <h1>{{ Posting.title}}</h1>
-        <p>投稿者:{{ Posting.user.name }}</p>
-        <p>投稿日時:{{ Posting.date }}{{ Posting.time }}</p>
-        <h3>{{ Posting.body }}</h3>
-      </ul>
+      <v-card-title>
+        <v-avatar color="blue" tile>
+          <v-icon large dark>mdi-account-circle</v-icon>
+        </v-avatar>
+        <span class="title font-weight-light">&nbsp;{{ Posting.user.name }}</span>
+      </v-card-title>
+
+      <v-card-title><span class="title font-weight-bold">{{ Posting.title }}</span></v-card-title>
+
+
+      <v-card-text class="headline font-weight-bold">
+      {{ Posting.title}}
+      <p>投稿日時:{{ Posting.date }}{{ Posting.time }}</p>
+      <h3>{{ Posting.body }}</h3>
+      </v-card-text>
+
     </v-card>
   </v-app>
 </div>
