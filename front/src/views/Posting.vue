7dd76061da24bc367a814b423b8c0f97bb594c89
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-02-19 15:31:43 +0900

    Add：投稿詳細にリンクを追加

--- front/src/views/Posting.vue
+++ front/src/views/Posting.vue
@@ -3,17 +3,24 @@
   <v-app>
     <v-card width="800px" class="mx-auto mt-10">
       <v-card-title>
-          <v-col :cols="1">
-        <v-avatar color="blue" tile>
-          <v-icon large dark>mdi-account-circle</v-icon>
-        </v-avatar>
-      </v-col>
-      <v-col :cols="6">
-        <span class="title font-weight-light">{{ Posting.user.name }}</span>
-      </v-col>
-      <v-col :cols="5">
-        <v-card-subtitle>{{ Posting.date }}{{ Posting.time }}に投稿</v-card-subtitle>
-      </v-col>
+
+        <v-col :cols="1">
+          <router-link to="/user">
+            <v-avatar color="blue" tile>
+              <v-icon large dark>mdi-account-circle</v-icon>
+            </v-avatar>
+          </router-link>
+        </v-col>
+
+        <v-col :cols="6">
+          <router-link to="/user">
+            <span class="title font-weight-light">{{ Posting.user.name }}</span>
+          </router-link>
+        </v-col>
+
+        <v-col :cols="5">
+          <v-card-subtitle>{{ Posting.date }}{{ Posting.time }}に投稿</v-card-subtitle>
+        </v-col>
       </v-card-title>
 
       <v-card-title><span class="headline font-weight-bold">{{ Posting.title }}</span></v-card-title>
