f6e61f64d0ca589b257a203a99beb49a1e816bed
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-02-27 00:08:40 +0900

    Add：Userのnameの一意性も追加

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
 
   # name は空なしで最大50文字
-  validates :name, presence: true, length: { maximum: 50}
+  validates :name, presence: true, length: { maximum: 50}, uniqueness: true
 
   # emailは空なしでvalidatesは /back/config/initializers/devise.rb で定義
   # passwordも空なしで email と同じところで定義されている
