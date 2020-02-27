731e8b7090723ade8c8a0e3553c8e42e6bbed048
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-02-27 15:24:02 +0900

    Add：vee-validateの環境構築

--- front/src/main.js
+++ front/src/main.js
@@ -5,6 +5,22 @@ import router from './router'
 import vuetify from './plugins/vuetify';
 import store from './store'
 
+import { extend, localize, ValidationProvider, ValidationObserver } from 'vee-validate';
+import ja from "vee-validate/dist/locale/ja.json"
+import { required, max, min, email, confirmed } from 'vee-validate/dist/rules';// バリデーションルール
+
+// VeeValidateがデフォルトで用意している各ルールを使用するよう指定
+extend("required", required) // 必須項目のバリデーション
+extend("email", email) // emailのバリデーション
+extend("max", max) // 最大文字数のバリデーション
+extend("min", min) // 最小文字数のバリデーション
+extend("confirmed", confirmed) // パスワード確認で同一か調べるバリデーション
+
+Vue.component("ValidationProvider", ValidationProvider)
+Vue.component("ValidationObserver", ValidationObserver)
+localize("ja", ja)
+
+
 Vue.config.productionTip = false
 
 new Vue({
