9cfd5a07435891277e10bd86d0151c291a2baca2
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-03-05 21:00:29 +0900

    Command：rails generate model Comment

--- back/app/controllers/api/post/comments_controller.rb
+++ back/app/controllers/api/post/comments_controller.rb
@@ -1,2 +1,9 @@
 class Api::Post::CommentsController < ApplicationController
+
+  def create
+    comment = Comment.new
+    comment.user_id = params[:user_id]
+    comment.question_id = params[:question_id]
+    comment.content = params[:content]
+  end
 end
