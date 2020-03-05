6ed07ae3f6f278f9d082e8429d00ff3108ab48e5
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-03-05 21:06:55 +0900

    Command：マイグレーション

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_094824) do
+ActiveRecord::Schema.define(version: 2020_03_05_115947) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "question_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
