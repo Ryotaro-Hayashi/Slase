d1e38e6d15c56695549e5184df8478b112954b44
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-03-05 15:29:11 +0900

    Command：rails generate controller api/post/comments

---
+++ back/app/controllers/api/post/comments_controller.rb
@@ -0,0 +1,2 @@
+class Api::Post::CommentsController < ApplicationController
+end
---
+++ back/test/controllers/api/post/comments_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::Post::CommentsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
