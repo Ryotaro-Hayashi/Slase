ccbb077f315f05d71f5e17e2e36de7dea5c7aff0
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-03-05 21:21:48 +0900

    Add：Commentを作成後の処理を追加

--- back/app/controllers/api/post/comments_controller.rb
+++ back/app/controllers/api/post/comments_controller.rb
@@ -5,5 +5,11 @@ class Api::Post::CommentsController < ApplicationController
     comment.user_id = params[:user_id]
     comment.question_id = params[:question_id]
     comment.content = params[:content]
+
+    question = Question.find_by(id: params[:question_id])
+    if question.save
+      render json: question
+    end
   end
+  
 end
