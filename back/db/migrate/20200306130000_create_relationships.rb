9e9b13414dac2e71864e26938694f8d33a880343
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-03-06 22:20:16 +0900

    Add：relationshipモデルのマイグレーションファイルを設定
    外部制約キー（カラムに格納できる値を他のテーブルの値に限定するもの）をありにする

--- back/db/migrate/20200306130000_create_relationships.rb
+++ back/db/migrate/20200306130000_create_relationships.rb
@@ -1,8 +1,13 @@
 class CreateRelationships < ActiveRecord::Migration[5.2]
   def change
     create_table :relationships do |t|
+      t.references :user, foreign_key: true
+      t.references :follow, foreign_key: { to_table: :users }
 
       t.timestamps
+
+      # user_id と follow_id のペアで重複するものが保存されないために設定
+      t.index [:user_id, :follow_id], unique: true
     end
   end
 end
