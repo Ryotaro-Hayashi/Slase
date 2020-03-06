5a07dd540bca27ca43ca93ca05ec8a727c018935
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-03-06 20:38:58 +0900

    Add：dateとtimeカラムをコメントモデルに追加してマイグレーション

--- back/db/migrate/20200305115947_create_comments.rb
+++ back/db/migrate/20200305115947_create_comments.rb
@@ -3,6 +3,9 @@ class CreateComments < ActiveRecord::Migration[5.2]
     create_table :comments do |t|
       t.string :content, null: false
 
+      t.string :date
+      t.string :time
+
       t.references :user
       t.references :question
 
--- back/db/schema.rb
+++ back/db/schema.rb
@@ -14,6 +14,8 @@ ActiveRecord::Schema.define(version: 2020_03_05_115947) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content", null: false
+    t.string "date"
+    t.string "time"
     t.bigint "user_id"
     t.bigint "question_id"
     t.datetime "created_at", null: false
