d41454296191e350971394057ba0974a3bb1c322
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-03-07 22:43:48 +0900

    Add：フォローとフォロワーを表示するページを追加してパスを通す

--- front/src/router.js
+++ front/src/router.js
@@ -11,6 +11,8 @@ import DetailPost from './views/DetailPost'
 import DetailUser from './views/DetailUser'
 import MyEmail from './views/MyEmail'
 import MyPassword from './views/MyPassword'
+import Follow from './views/Follow'
+import Follower from './views/Follower'
 
 Vue.use(Router)
 
@@ -66,6 +68,16 @@ export default new Router({
       path: '/mypage/password',
       name: 'MyPasword',
       component: MyPassword
+    },
+    {
+      path: '/follow',
+      name: 'Follow',
+      component: Follow
+    },
+    {
+      path: '/follower',
+      name: 'Follower',
+      component: Follower
     }
   ]
 })
---
+++ front/src/views/Follower.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="Follower">
+    <v-app>
+
+      <v-card width="600px" class="mx-auto mt-10 mb-10">
+        <!-- 表示切り替え -->
+        <v-card-title>
+          <v-icon class="icon-space">mdi-format-list-bulleted-square</v-icon>
+          <span class="title font-weight-bold">latest</span>
+        </v-card-title>
+
+        <v-divider></v-divider>
+
+        <v-card-text>
+          <v-list three-line>
+            <template v-for="post in allPosts">
+              <v-list-item :key="post.id">
+                <!-- アバター -->
+                <v-list-item-avatar color="blue" tile>
+                  <v-icon large dark>mdi-account-circle</v-icon>
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                  <!-- タイトル -->
+                  <v-list-item-title>
+                    <router-link class="title font-weight-bold" to="/detail/post" @click.native="getDetailPost(post.id)">{{ post.title }}</router-link>
+                  </v-list-item-title>
+                  <!-- 投稿者と投稿日時 -->
+                  <v-list-item-subtitle>
+                    <v-row>
+                      <v-col>投稿者：<router-link to="/detail/user" @click.native="getDetailUserPosts(post.user)">{{ post.user.name }}</router-link></v-col>
+                      <v-col>投稿日時：{{ post.date }}</v-col>
+                    </v-row>
+                  </v-list-item-subtitle>
+
+                  <v-divider></v-divider>
+
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+          </v-list>
+        </v-card-text>
+
+      </v-card>
+
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Follower',
+  data () {
+    return {
+      allPosts: ''
+    }
+  },
+  methods: {
+    // 投稿一覧を取得
+    getAllPosts () {
+      this.$http.get('http://localhost:3000/api/post/questions')
+      .then(response => {
+        if (response.status === 200) {
+          this.allPosts = response.data
+        }
+      })
+    },
+    // 投稿の詳細を取得
+    getDetailPost (id) {
+      this.$store.dispatch("post/getDetailPost", id)
+    },
+    // 詳細表示するユーザーの投稿一覧を取得
+    getDetailUserPosts (user) {
+      this.$store.dispatch("post/getDetailUserPosts", user.id)
+      // 詳細表示しているユーザーを更新
+      this.$store.commit("auth/changeDetailUser", user)
+    }
+  },
+  // マウント時にステートの投稿一覧を更新
+  mounted () {
+    this.getAllPosts();
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
