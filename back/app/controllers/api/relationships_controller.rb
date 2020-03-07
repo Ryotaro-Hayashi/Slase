43b1a892bd55a880fb193a664d6a90df7bbff329
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-03-07 20:14:31 +0900

    Add：フォローのためのアクションを追加

--- back/app/controllers/api/relationships_controller.rb
+++ back/app/controllers/api/relationships_controller.rb
@@ -1,2 +1,17 @@
 class Api::RelationshipsController < ApplicationController
+  def create
+    user = User.find(params[:relationship][:follow_id])
+    following = current_api_user.follow(user)
+    if following.save
+      render json: following
+    end
+  end
+
+  def destroy
+    user = User.find(params[:relationship][:follow_id])
+    following = current_user.unfollow(user)
+    if following.destroy
+      render json: following
+    end
+  end
 end
