c803120260c970069de4e9069ea76ccf4cf41db1
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-03-16 15:52:14 +0900

    Add：serializerにプロフィール情報を追加

--- back/app/serializers/user_serializer.rb
+++ back/app/serializers/user_serializer.rb
@@ -3,6 +3,10 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id,
              :name,
              :email,
+             :introduce,
+             :age,
+             :sex,
+             :address,
              :questions
 
   has_many :questions
