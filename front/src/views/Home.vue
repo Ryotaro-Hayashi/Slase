fdb85a0322ccbca1f4572b857c8dce67e4a12039
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-03-25 05:57:51 +0900

    Fix：クリックによって投稿をソート

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -4,38 +4,40 @@
 
       <v-container>
         <v-row>
+          <!-- スマホ以外のときのソートのためのナビゲーション -->
           <v-col cols="0" sm="3" md="2" lg="2" v-if="loggedIn && !$vuetify.breakpoint.xsOnly">
+            <!-- タイムライン -->
+            <v-list-item>
+              <v-fab-transition>
+                <v-btn color="deep-purple" dark fixed width="170px" @click="getPosts(states[0])">
+                  <span class="mr-auto">
+                    <v-icon>mdi-format-list-bulleted-square</v-icon>タイムライン
+                  </span>
+                </v-btn>
+              </v-fab-transition>
+            </v-list-item>
 
-              <v-list-item>
-                <v-fab-transition>
-                  <v-btn color="deep-purple" dark fixed width="170px" @click="getPosts">
-                    <span class="mr-auto">
-                      <v-icon>mdi-format-list-bulleted-square</v-icon>タイムライン
-                    </span>
-                  </v-btn>
-                </v-fab-transition>
-              </v-list-item>
+            <!-- 全ての投稿 -->
+            <v-list-item>
+              <v-fab-transition>
+                <v-btn color="deep-purple" dark fixed width="170px" @click="getPosts(states[1])">
+                  <span class="mr-auto">
+                    <v-icon>mdi-format-list-bulleted-square</v-icon>全ての投稿
+                  </span>
+                </v-btn>
+              </v-fab-transition>
+            </v-list-item>
 
-              <v-list-item>
-                <v-fab-transition>
-                  <v-btn color="deep-purple" dark fixed width="170px" @click="getPosts">
-                    <span class="mr-auto">
-                      <v-icon>mdi-format-list-bulleted-square</v-icon>全ての投稿
-                    </span>
-                  </v-btn>
-                </v-fab-transition>
-              </v-list-item>
-
-              <v-list-item>
-                <v-fab-transition>
-                  <v-btn color="deep-purple" dark fixed width="170px" @click="getPosts">
-                    <span class="mr-auto">
-                      <v-icon>mdi-format-list-bulleted-square</v-icon>いいねした投稿
-                    </span>
-                  </v-btn>
-                </v-fab-transition>
-              </v-list-item>
-
+            <!-- いいねした投稿 -->
+            <v-list-item>
+              <v-fab-transition>
+                <v-btn color="deep-purple" dark fixed width="170px" @click="getPosts(states[2])">
+                  <span class="mr-auto">
+                    <v-icon>mdi-format-list-bulleted-square</v-icon>いいねした投稿
+                  </span>
+                </v-btn>
+              </v-fab-transition>
+            </v-list-item>
           </v-col>
 
           <v-col cols="12" sm="9" md="10" lg="8">
@@ -43,17 +45,16 @@
               <!-- 表示切り替え -->
               <v-card-title>
                 <v-row align=center>
+
+                  <!-- ソートの種類のアイコン -->
                   <v-col cols="2" sm="1" md="1" lg="1">
-                    <!-- ログイン時のタイトル -->
-                    <!-- <v-select v-model="type" :items="states" prepend-icon="mdi-format-list-bulleted-square" single-line @change="getPosts" v-if="loggedIn"></v-select> -->
-                    <!-- ログアウト時のタイトル -->
-                      <v-icon large>mdi-format-list-bulleted-square</v-icon>
-                    <!-- </span> -->
+                    <v-icon large>mdi-format-list-bulleted-square</v-icon>
                   </v-col>
 
                   <!-- ソートの種類 -->
                   <v-col cols="10" sm="6" md="6" lg="6">
-                    <span class="title font-weight-bold">タイムライン</span>
+                    <span class="title font-weight-bold" v-if="loggedIn">{{ sort }}</span>
+                    <span class="title font-weight-bold" v-if="!loggedIn">{{ type }}</span>
                   </v-col>
                 </v-row>
               </v-card-title>
@@ -123,12 +124,12 @@ export default {
   data () {
     return {
       allPosts: '',
-      // 投稿表示の切り替え
-      type: 'フォローユーザーの投稿',
       // 投稿表示の選択肢
       states: [
-        'フォローユーザーの投稿', '全ての投稿', 'goodした投稿'
+        'タイムライン', '全ての投稿', 'いいねした投稿'
       ],
+      sort: "タイムライン",
+      type: "全ての投稿"
     }
   },
   computed: {
@@ -196,10 +197,11 @@ export default {
       this.$store.dispatch("user/getDetailUser", id)
     },
     // 投稿一覧のソート
-    getPosts () {
-      if (this.type === '全ての投稿') {
+    getPosts (type) {
+      this.sort = type
+      if (this.sort === '全ての投稿') {
         this.getAllPosts ()
-      } else if (this.type === 'フォローユーザーの投稿') {
+      } else if (this.sort === 'フォローユーザーの投稿') {
         this.getFollowingsPosts ()
       } else {
         this.getLikedPosts ()
